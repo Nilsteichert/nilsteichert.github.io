4f9627c245256f2adf7927329d14fe80f46c18c8
Nils Teichert <nils.teichert1@gmail.com>
2021-02-01 00:03:27 +0100

    implemented automatic midi listening

--- js/musicapp.js
+++ js/musicapp.js
@@ -9,17 +9,55 @@ class Musicapp{
         this.noteDrawer = new NoteDrawer(this.drawDiv,this.note,this.keySignature.keySignature)
         this.noteChecker = new NoteChecker(this.keySignature);
         this.animator = new Animator(this.drawDiv);
+
+        this.enableMidi(() => {this.listenToMidi()})
     }
 
     draw(){this.noteDrawer.drawOneNote(this.note);}
-    nextNote(){this.note = new Note(this.minNote,this.maxNote); this.draw();}
+    nextNote(){
+        this.note = new Note(this.minNote,this.maxNote); this.draw();
+    }
     selectKeySignature(keySignature){
         this.keySignature = new KeySignature(keySignature);
         this.noteChecker = new NoteChecker(this.keySignature);
         this.noteDrawer= new NoteDrawer(this.drawDiv,this.note,this.keySignature.keySignature);
         this.draw();
     }
-    animate(color){this.animator.startAnimation(color)};
-    
+    rightNote(){
+        this.animator.startAnimation("green");
+        this.nextNote();
+    }
+    wrongNote(){this.animator.startAnimation("red");}
 
+
+    // MIDI handling
+
+    enableMidi(callback) {
+        WebMidi.enable(function (err) {
+            //Enable WebMidi
+            if (err) {
+                // An error occured while starting WebMidi
+                console.log("WebMidi could not be enabled.", err);
+                window.alert("WebMidi is not supported by your Browser.")
+            } 
+            else {
+                // WebMidi enabled successfully
+                callback();
+            } 
+        });
+    }
+
+    listenToMidi()
+    {
+        WebMidi.inputs.forEach(input => {
+            input.addListener('noteon', "all", (e) => {
+                var receivedNote = e.note.name + "/" + e.note.octave;
+                console.log("Received 'noteon' message (" + receivedNote + ").");
+                if(this.noteChecker.checkNote(this.note,receivedNote)){
+                    this.rightNote()}
+                else(this.wrongNote())
+              })          
+        });
+    }
+    disableMidi() {WebMidi.inputs.forEach(x => {x.removeListener()})}
 }
