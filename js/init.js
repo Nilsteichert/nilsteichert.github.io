de56fe193fc89b55fb79dc291a8cf11ae64f775f
Nils Teichert <nils.teichert1@gmail.com>
2021-02-04 16:28:00 +0100

    changed standard range

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 const cName = "app";
 // navigator.requestMIDIAccess()
 
-app = new Musicapp(undefined, "C/4", undefined, "noteDraw");
+app = new Musicapp(undefined, undefined, undefined, "noteDraw");
 app.draw();
