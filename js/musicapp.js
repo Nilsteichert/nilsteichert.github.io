44872f5f5ee038a43b3145c6436e1780573b135b
Nils Teichert <nils.teichert1@gmail.com>
2021-03-01 17:50:18 +0100

    added + - buttons

--- js/musicapp.js
+++ js/musicapp.js
@@ -18,6 +18,7 @@ class Musicapp {
     );
     this.noteChecker = new NoteChecker(this.keySignature);
     this.animator = new Animator(this.drawDiv);
+    this.rangeModalHandler = new RangeModalHandler();
     this.tuner;
     this.tunerEnabled = false;
     this.addFlats;
@@ -182,46 +183,5 @@ class Musicapp {
     }
     this.note = new Note(this.note.note, this.note.note, setTo);
     this.draw();
-  }
-
-  //Range Modal:
-  noteInRange(input) {
-    const validNotes = ["C", "D", "E", "F", "G", "A", "B"];
-    if (validNotes.includes(input.charAt(0))) {
-      let octave = parseInt(input.charAt(1));
-      if (octave > 0 && octave < 9) {
-        return true;
-      }
-    }
-    return false;
-  }
-  validateInput() {
-    const inputMin = document.getElementById("inputMin");
-    const inputMax = document.getElementById("inputMax");
-
-    const updateValueMin = (e) => {
-      var input = e.target.value;
-
-      if (this.noteInRange(input)) {
-        inputMin.classList.remove("is-invalid");
-        inputMin.classList.add("is-valid");
-      } else {
-        inputMin.classList.remove("is-valid");
-        inputMin.classList.add("is-invalid");
-      }
-    };
-    const updateValueMax = (e) => {
-      var input = e.target.value;
-
-      if (this.noteInRange(input)) {
-        inputMax.classList.remove("is-invalid");
-        inputMax.classList.add("is-valid");
-      } else {
-        inputMax.classList.remove("is-valid");
-        inputMax.classList.add("is-invalid");
-      }
-    };
-    inputMin.addEventListener("input", updateValueMin);
-    inputMax.addEventListener("input", updateValueMax);
   }
 }
---
+++ js/rangeModalHandler.js
@@ -0,0 +1,141 @@
+class RangeModalHandler {
+  constructor() {
+    this.validNotes = ["C", "D", "E", "F", "G", "A", "B"];
+    this.allNotes = {};
+    this.inputMin = document.getElementById("inputMin");
+    this.inputMax = document.getElementById("inputMax");
+  }
+
+  noteInRange(input) {
+    if (this.validNotes.includes(input.charAt(0))) {
+      let octave = parseInt(input.charAt(1));
+      if (octave > 0 && octave < 9) {
+        return true;
+      }
+    }
+    return false;
+  }
+  validateInput() {
+    const updateValueMin = (e) => {
+      var input = e.target.value;
+
+      if (this.noteInRange(input)) {
+        this.inputMin.classList.remove("is-invalid");
+        this.inputMin.classList.add("is-valid");
+      } else {
+        this.inputMin.classList.remove("is-valid");
+        this.inputMin.classList.add("is-invalid");
+      }
+    };
+    const updateValueMax = (e) => {
+      var input = e.target.value;
+
+      if (this.noteInRange(input)) {
+        this.inputMax.classList.remove("is-invalid");
+        this.inputMax.classList.add("is-valid");
+      } else {
+        this.inputMax.classList.remove("is-valid");
+        this.inputMax.classList.add("is-invalid");
+      }
+    };
+    this.inputMin.addEventListener("input", updateValueMin);
+    this.inputMax.addEventListener("input", updateValueMax);
+  }
+
+  increaseNote(note) {
+    if (!this.noteInRange(note)) {
+      return "C4";
+    }
+    if (note == "B8") {
+      return note;
+    }
+    var noteValue = note.charAt(0);
+    var noteOctave = parseInt(note.charAt(1));
+    switch (noteValue) {
+      case "C":
+        return `D${noteOctave}`;
+
+      case "D":
+        return `E${noteOctave}`;
+
+      case "E":
+        return `F${noteOctave}`;
+
+      case "F":
+        return `G${noteOctave}`;
+
+      case "G":
+        return `A${noteOctave}`;
+
+      case "A":
+        return `B${noteOctave}`;
+
+      case "B":
+        return `C${noteOctave + 1}`;
+
+      default:
+        break;
+    }
+  }
+
+  decreaseNote(note) {
+    if (!this.noteInRange(note)) {
+      return "C4";
+    }
+    if (note == "C1") {
+      return note;
+    }
+    var noteValue = note.charAt(0);
+    var noteOctave = parseInt(note.charAt(1));
+    switch (noteValue) {
+      case "C":
+        return `B${noteOctave - 1}`;
+
+      case "D":
+        return `C${noteOctave}`;
+
+      case "E":
+        return `D${noteOctave}`;
+
+      case "F":
+        return `E${noteOctave}`;
+
+      case "G":
+        return `F${noteOctave}`;
+
+      case "A":
+        return `G${noteOctave}`;
+
+      case "B":
+        return `A${noteOctave}`;
+
+      default:
+        break;
+    }
+  }
+  increaseMin() {
+    this.inputMin.value = this.increaseNote(this.inputMin.value);
+    this.inputMin.classList.remove("is-invalid");
+    this.inputMin.classList.add("is-valid");
+  }
+  increaseMax() {
+    this.inputMax.value = this.increaseNote(this.inputMax.value);
+    this.inputMin.classList.remove("is-invalid");
+    this.inputMin.classList.add("is-valid");
+  }
+
+  decreaseMin() {
+    this.inputMin.value = this.decreaseNote(this.inputMin.value);
+    this.inputMin.classList.remove("is-invalid");
+    this.inputMin.classList.add("is-valid");
+  }
+  decreaseMax() {
+    this.inputMax.value = this.decreaseNote(this.inputMax.value);
+    this.inputMin.classList.remove("is-invalid");
+    this.inputMin.classList.add("is-valid");
+  }
+
+  compareMinMax(minNote, maxNote) {
+    //TODO
+  }
+}
