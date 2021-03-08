998bc11f2a7135b2571749db5149259c5b7c9136
Nils Teichert <nils.teichert1@gmail.com>
2021-03-08 23:16:14 +0100

    fix

---
+++ js/level.js
@@ -0,0 +1,12 @@
+const countedNotes = 100; // How many notes get counted for average time
+
+//Average response times for each level
+const rtBeginner = 10000;
+const rtSemiIntermediate = 8000;
+const rtIntermediate = 5000;
+const rtAdvanced = 3000;
+const rtProfessional = 1000;
+
+class Level {
+  constructor() {}
+}
